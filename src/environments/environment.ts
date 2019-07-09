e06a76a39cdd03b2f35732061cd89f2d71b67034
catbutt2019 <c13453402@mydit.ie>
2019-07-09 14:41:32 +0100

    hi

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCnZsAhJ0YuDsvoaYWA62jZH121ROO8u9c",
-    authDomain: "wippycatherine.firebaseapp.com",
-    databaseURL: "https://wippycatherine.firebaseio.com",
-    projectId: "wippycatherine",
-    storageBucket: "wippycatherine.appspot.com",
-    messagingSenderId: "700698664500",
-    appId: "1:700698664500:web:96e4e480968bb27c"
+    apiKey: "AIzaSyChkwA9qwuX8DWJb5m0Gjnsiqgsj0377Uc",
+    authDomain: "wippy-1962c.firebaseapp.com",
+    databaseURL: "https://wippy-1962c.firebaseio.com",
+    projectId: "wippy-1962c",
+    storageBucket: "wippy-1962c.appspot.com",
+    messagingSenderId: "115626323332",
+    appId: "1:115626323332:web:84270a7a02c1607b"
   }
 };
 
