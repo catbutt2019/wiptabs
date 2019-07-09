eb7171da5bb8817bd8c4acd846ff479ea7b2f987
catbutt2019 <c13453402@mydit.ie>
2019-07-09 14:34:31 +0100

    hello

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,27 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+ 
+import * as firebase from 'firebase';
+import { AuthenticateService } from './services/authentication.service';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+      BrowserModule,
+      IonicModule.forRoot(),
+      AppRoutingModule,
+      ReactiveFormsModule,
+      AngularFireAuthModule
+    ],
   providers: [
     StatusBar,
     SplashScreen,
+    AuthenticateService,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCnZsAhJ0YuDsvoaYWA62jZH121ROO8u9c",
+    authDomain: "wippycatherine.firebaseapp.com",
+    databaseURL: "https://wippycatherine.firebaseio.com",
+    projectId: "wippycatherine",
+    storageBucket: "wippycatherine.appspot.com",
+    messagingSenderId: "700698664500",
+    appId: "1:700698664500:web:96e4e480968bb27c"
+  }
 };
 
 /*
