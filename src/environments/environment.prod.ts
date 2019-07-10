af84790930094fc68aa890284acd85eec548da74
catbutt2019 <c13453402@mydit.ie>
2019-07-10 15:25:48 +0100

    first test on xcode worked

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyChkwA9qwuX8DWJb5m0Gjnsiqgsj0377Uc",
+    authDomain: "wippy-1962c.firebaseapp.com",
+    databaseURL: "https://wippy-1962c.firebaseio.com",
+    projectId: "wippy-1962c",
+    storageBucket: "wippy-1962c.appspot.com",
+    messagingSenderId: "115626323332",
+    appId: "1:115626323332:web:84270a7a02c1607b"
+  }
 };
