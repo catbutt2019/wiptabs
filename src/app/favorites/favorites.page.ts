0c68e32ec536d39ddb9b0a62047cf30b6e47a751
catbutt2019 <c13453402@mydit.ie>
2019-11-14 21:07:21 +0000

    favorites added

--- src/app/favorites/favorites.page.ts
+++ src/app/favorites/favorites.page.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild  } from '@angular/core';
+import { NavController, ModalController } from '@ionic/angular';
+import { AuthenticateService } from '../services/authentication.service';
+import { LoadingController } from '@ionic/angular';
+import { Router, ActivatedRoute } from '@angular/router';
+import { IonSlides } from '@ionic/angular';
+import { EventService } from '../services/event.service';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/storage';
+import { ProfileService } from '../services/profile.service';
+import { FirebaseService } from '../services/firebase.service';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { FavoriteService } from '../services/favorite.service';
 
 @Component({
   selector: 'app-favorites',
@@ -7,9 +20,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoritesPage implements OnInit {
 
-  constructor() { }
+  favoriteList = []; 
+  public favoriteListRef: firebase.firestore.CollectionReference;
+
+  constructor(
+    private firestore: AngularFirestore,
+    private navCtrl: NavController,
+    private authService: AuthenticateService,
+    public loadingCtrl: LoadingController,
+    private router: Router,
+    private route: ActivatedRoute,
+    private eventService: EventService,
+    private favoriteService: FavoriteService,
+    private profileService: ProfileService,
+    private firebaseService: FirebaseService,
+  ) { }
 
   ngOnInit() {
+
+    this.favoriteService.getfavoriteList().then(favoriteListSnapshot => {
+      this.favoriteList = [];
+      favoriteListSnapshot.forEach(snap => {
+        this.favoriteList.push({
+          id: snap.id,
+          favoriteList: snap.data().favourite
+        });
+        console.log(snap.data().favourite)
+        return false;
+      });
+    }); 
+    
   }
+
+ 
 
 }
